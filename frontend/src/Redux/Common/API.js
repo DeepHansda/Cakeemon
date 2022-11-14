96f763de642a4ba30dc64482f4ecd7a6ef47ab03
Deep Hansda <deephansda921@gmail.com>
2022-11-14 09:36:56 +0530

    working on banners

--- frontend/src/Redux/Common/API.js
+++ frontend/src/Redux/Common/API.js
@@ -1,17 +1,13 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials=true
 // https://cakeemon-backend.vercel.app/
 // http://localhost:3400/api
 export const API = axios.create({
   baseURL: "https://cakeemon-backend.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": [
-      "https://cakeemon.vercel.app",
-      "http://localhost:3400",
-    ],
-    "Access-Control-Allow-Headers":
-      "Origin X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept Accept-Encoding",
-  },
+    "Access-Control-Allow-Origin": ["https://cakeemon.vercel.app","http://localhost:3400"],
+    'Access-Control-Allow-Headers' : 'Origin X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept Accept-Encoding',
+    },
 });
